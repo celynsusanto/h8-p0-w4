84fb663b18b91ee022753fe68af7b12a74fd9e9e
Celyn Susanto <celyn.vy@gmail.com>
2018-09-18 14:15:36 +0700

    week4

--- Exercise 3 Cari Median.js
+++ Exercise 3 Cari Median.js
@@ -6,15 +6,14 @@
 
 //1.Masuk kedalam fungsi cariMedian(arr)
 //2.Buat variable 'median' dengan nilai 0
-//3.Urutkan 'arr' dari angka terkecil sampai angka terbesar
-//4.Buat kondisi dimana Jika panjang 'arr' modulus 2 tidak sama dengan 0 
-//  4.a.Tampilkan nilai 'median' sama dengan 'arr' di index bagi 2 dan dibulatkan kebawah
-//5.Buat kondisi kedua dimana Jika panjang 'arr' modulus 2 sama dengan 0
-//  5.a.Buat variable 'nilaiAtas' dengan nilai 'arr' dengan index panjang 'arr' dibagi 2
-//  5.b.Buat variable 'nilaiBawah' dengan nilai 'arr' dengan index panjang 'arr' dibagi 2 dan dikurangi 1
-//  5.c.'median' sama dengan 'nilaiBawah' ditambah 'nilaiAtas' dibagi 2
-//6.Kembalikan nilai 'median'
-//7.Selesai
+//3.Buat kondisi dimana Jika panjang 'arr' modulus 2 tidak sama dengan 0 
+//  3.a.Tampilkan nilai 'median' sama dengan 'arr' di index bagi 2 dan dibulatkan kebawah
+//4.Buat kondisi kedua dimana Jika panjang 'arr' modulus 2 sama dengan 0
+//  4.a.Buat variable 'nilaiAtas' dengan nilai 'arr' dengan index panjang 'arr' dibagi 2
+//  4.b.Buat variable 'nilaiBawah' dengan nilai 'arr' dengan index panjang 'arr' dibagi 2 dan dikurangi 1
+//  4.c.'median' sama dengan 'nilaiBawah' ditambah 'nilaiAtas' dibagi 2
+//5.Kembalikan nilai 'median'
+//6.Selesai
 
 function cariMedian(arr) {
     var median = 0
