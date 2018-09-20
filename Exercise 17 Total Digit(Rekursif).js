0333088134262e33647fe36660e176ce745abbd4
Celyn Susanto <celyn.vy@gmail.com>
2018-09-20 16:08:35 +0700

    week4

--- Exercise 17 Total Digit(Rekursif).js
+++ Exercise 17 Total Digit(Rekursif).js
@@ -3,20 +3,28 @@ function totalDigitRekursif(angka)
 return nilai dr total digit tsb dr dpn ke blkg
 */
 function totalDigitRekursif(angka) {
-  var index = 0;
+
   var jumlah = 0;
-  if (index===angka.toString().length-1) {
-    jumlah = jumlah + parseInt(angka.toString()[index]);
-    return jumlah;
+  if (angka.toString().length==0) {
+    return Number(angka);
   } else {
-    jumlah = jumlah + parseInt(angka.toString()[index]);
-    index++;
-    return jumlah + totalDigitRekursif(angka.toString().length--);
+    jumlah=parseInt(angka.toString()[0])
+    //console.log(jumlah + '+' + angka.toString().slice(1));
+    return jumlah+totalDigitRekursif(angka.toString().slice(1))
   }
+  
 }
+//pakai slice
 
 console.log(totalDigitRekursif(512)); // 8
 console.log(totalDigitRekursif(1542)); // 12
 console.log(totalDigitRekursif(5)); // 5
 console.log(totalDigitRekursif(21)); // 3
 console.log(totalDigitRekursif(11111)); // 5
+
+/**
+ * 512
+  return 5 + rec(12)
+    return 1 + rec(2)
+      return 2
+ */
